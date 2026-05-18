67bcca2322de507c60f8522f24b8f30406a1dc21
James Schulz <jamesschulzm@gmail.com>
2026-05-18 18:41:39 -0300

    [ADD] Criando a primeira rota

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Login } from './pages/Login/index.jsx'
-import { Register } from './pages/Register/index.jsx'
-import { Feed } from './pages/Feed/index.jsx'
-import { BlogPost } from './pages/BlogPost/index.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
+import { BlogPost } from "./pages/BlogPost/index.jsx";
+import { Feed } from "./pages/Feed/index.jsx";
+import { Login } from "./pages/Login/index.jsx";
+import { Register } from "./pages/Register/index.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Login />
-    {/* <Register /> */}
-    {/* <Feed /> */}
-    {/* <BlogPost /> */}
+    <BrowserRouter>
+      {/* Define as rotas da aplicação */}
+      <Routes>
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/" element={<Feed />} />
+        <Route path="/blog-post" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
