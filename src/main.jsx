4822d0cbb350968bf66afbb0d1d6d087f5c16156
James Schulz <jamesschulzm@gmail.com>
2026-06-09 18:17:14 -0300

    [REF] Organizando as rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { ProtectedRoute } from "./components/ProtectedRoute/index.jsx";
 import "./index.css";
-import { BlogPost } from "./pages/BlogPost/index.jsx";
-import { Feed } from "./pages/Feed/index.jsx";
-import { Login } from "./pages/Login/index.jsx";
-import { Logout } from "./pages/Logout/index.jsx";
-import { Register } from "./pages/Register/index.jsx";
+import { AppRouter } from "./router/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      {/* Define as rotas da aplicação */}
-      <Routes>
-        <Route path="/auth">
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="logout" element={<Logout />} />
-        </Route>
-
-        <Route path="/">
-          <Route
-            path=""
-            element={
-              <ProtectedRoute>
-                <Feed />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="blog-post/:slug"
-            element={
-              <ProtectedRoute>
-                <BlogPost />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AppRouter />
   </StrictMode>,
 );
---
+++ src/router/index.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { ProtectedRoute } from "../components/ProtectedRoute/index.jsx";
+import { BlogPost } from "../pages/BlogPost/index.jsx";
+import { Feed } from "../pages/Feed/index.jsx";
+import { Login } from "../pages/Login/index.jsx";
+import { Logout } from "../pages/Logout/index.jsx";
+import { Register } from "../pages/Register/index.jsx";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      {/* Define as rotas da aplicação */}
+      <Routes>
+        <Route path="/auth">
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="logout" element={<Logout />} />
+        </Route>
+
+        <Route path="/">
+          <Route
+            path=""
+            element={
+              <ProtectedRoute>
+                <Feed />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="blog-post/:slug"
+            element={
+              <ProtectedRoute>
+                <BlogPost />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
